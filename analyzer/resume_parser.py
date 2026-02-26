9acba5b63c37114555b60a85388b561f78cfadf1
Priyam <141039169+Priyam2709@users.noreply.github.com>
2026-02-26 15:28:04 +0530

    Delete analyzer directory

--- analyzer/resume_parser.py
+++
@@ -1,8 +0,0 @@
-from pdfminer.high_level import extract_text
-
-def extract_resume_text(file_path):
-    try:
-        text = extract_text(file_path)
-        return text.lower()
-    except Exception as e:
-        return ""
--- analyzer/skill_embeddings.py
+++
@@ -1,8 +0,0 @@
-from .model_loader import get_model
-from .skill_extractor import SKILL_DB
-
-model = get_model()
-
-print("Generating skill embeddings... (only once)")
-
-SKILL_EMBEDDINGS = model.encode(SKILL_DB)
